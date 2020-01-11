ff1873a2a5faa08a5b9a7102be0e57c7fdfbbec1
hiturarael <hiturarael@hotmail.com>
2020-01-11 18:00:22 -0500

    Bot Connection

--- DiscordBots/BotBase.cs
+++ DiscordBots/BotBase.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace DiscordBots
 {
     public class BotBase
     {
         public string StrCmd { get; set; }
+        public string StrBotToken { get; set; }
+        private DiscordSocketClient _client;
 
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
+        => new BotBase().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
+            _client = new DiscordSocketClient();
 
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            //var token = "token";
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, StrBotToken);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
     }
 }
